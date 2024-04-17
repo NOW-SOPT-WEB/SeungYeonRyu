59184548a3a1bbf85c0f1cd15ecac75ef72f9f1c
wrryu09 <rryu09@ewhain.net>
2024-04-17 16:13:39 +0900

    feat: sildebar animation

--- week2/shoppingList/common/sidebar.js
+++ week2/shoppingList/common/sidebar.js
@@ -10,11 +10,11 @@ sideBarCartBtn.addEventListener("click", () => {
 // 햄버거 버튼으로 사이드바 열기
 const hamburgerIcon = document.querySelector("#hamburgerIcon");
 hamburgerIcon.addEventListener("click", () => {
-  sideBar.classList.toggle("invisible");
+  sideBar.classList.toggle("slide-invisible");
 });
 
 // 사이드바 닫기
 const sideBarClose = document.querySelector("#sidebar-close");
 sideBarClose.addEventListener("click", () => {
-  sideBar.classList.add("invisible");
+  sideBar.classList.add("slide-invisible");
 });

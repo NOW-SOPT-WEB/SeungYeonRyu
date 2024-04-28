80dd6db7c9baf1386252a77cccc8d22b51971bc1
wrryu09 <rryu09@ewhain.net>
2024-04-28 17:56:51 +0900

    style: levelbtn 위아래 Margin 추가

--- week3/src/components/LevelBtnGroup.tsx
+++ week3/src/components/LevelBtnGroup.tsx
@@ -16,5 +16,8 @@ const LevelBtnGroup = (props: Props) => {
 const BtnGroupContainer = styled.div`
   display: flex;
   gap: 0.6rem;
+
+  margin-top: 6rem;
+  margin-bottom: 2rem;
 `;
 export default LevelBtnGroup;

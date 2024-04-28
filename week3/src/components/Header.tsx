3980e2d8d9725f07b0fb1e85c58549500e24881e
wrryu09 <rryu09@ewhain.net>
2024-04-28 16:56:27 +0900

    feat: 헤더 퍼블리싱

--- week3/src/components/Header.tsx
+++ week3/src/components/Header.tsx
@@ -1,9 +1,33 @@
 import React from "react";
+import styled from "styled-components";
 
 type Props = {};
 
 const Header = (props: Props) => {
-  return <div>Header</div>;
+  return (
+    <HeaderContainer>
+      <HeaderH1>웨비의 카드 맞히기~</HeaderH1>
+      <HeaderScore>score</HeaderScore>
+    </HeaderContainer>
+  );
 };
 
+const HeaderContainer = styled.div`
+  height: 5rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  color: white;
+  background-color: ${({ theme }) => theme.colors.mainBlue};
+`;
+
+const HeaderH1 = styled.h1`
+  font-size: ${({ theme }) => theme.fontSizes.small};
+`;
+
+const HeaderScore = styled.p`
+  font-weight: 800;
+  font-size: ${({ theme }) => theme.fontSizes.medium};
+`;
 export default Header;
--- week3/src/pages/CardGame.tsx
+++ week3/src/pages/CardGame.tsx
@@ -1,13 +1,21 @@
 import React from "react";
 import Header from "../components/Header";
+import styled from "styled-components";
 
 type Props = {};
 
 const CardGame = (props: Props) => {
   return (
-    <>
+    <CardPageContainer>
       <Header />
-    </>
+    </CardPageContainer>
   );
 };
+
+/** 배경 */
+const CardPageContainer = styled.div`
+  width: 100%;
+  height: 100dvh;
+  background-color: ${({ theme }) => theme.colors.darkGrey};
+`;
 export default CardGame;

5babbe80603c96c82653cfdd7b20bc025ad263b0
wrryu09 <rryu09@ewhain.net>
2024-04-29 00:14:26 +0900

    feat: add card images

---
+++ week3/src/assets/cardImage/cardImage.ts
@@ -0,0 +1,21 @@
+import Bellsprout from "./Bellsprout.png";
+import Cleffa from "./Cleffa.png";
+import Clodsire from "./Clodsire.png";
+import Cosmog from "./Cosmog.png";
+import Cyndaquil from "./Cyndaquil.png";
+import Diglett from "./Diglett.png";
+import Mudkip from "./Mudkip.png";
+import Slowpoke from "./Slowpoke.png";
+import Darumaka from "./Darumaka.png";
+
+export const cardImage = {
+  cardImg1: Bellsprout,
+  cardImg2: Cleffa,
+  cardImg3: Clodsire,
+  cardImg4: Cosmog,
+  cardImg5: Cyndaquil,
+  cardImg6: Diglett,
+  cardImg7: Mudkip,
+  cardImg8: Slowpoke,
+  cardImg9: Darumaka,
+};

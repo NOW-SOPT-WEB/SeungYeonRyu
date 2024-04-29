3dcf4d890defb7b6e6091c00c1449e9a7845f0a6
wrryu09 <rryu09@ewhain.net>
2024-04-29 16:56:46 +0900

    fix: 같은 카드 다시 누르면 뒤집기

--- week3/src/components/CardGroup.tsx
+++ week3/src/components/CardGroup.tsx
@@ -17,14 +17,19 @@ const CardGroup = (props: Props) => {
   // 현재 선택한 카드
   const [current, setCurrent] = useState("-1");
   const handleCurrent = (cardId: string) => {
-    checkCardMatch(cardId) ? setCurrent("-1") : setCurrent(cardId);
+    if (cardId === current) {
+      // 같은 카드 다시 누르면 뒤집기
+      setCurrent("-1");
+    } else {
+      // 다른 카드인 경우 짝인지 확인
+      checkCardMatch(cardId) ? setCurrent("-1") : setCurrent(cardId);
+    }
   };
 
   /** 선택한 카드 맞았는지 확인 */
   const checkCardMatch = (cardId: string) => {
     // 이미 있는 경우
     if (correctCards.includes(cardId.split("_")[0])) {
-      console.log("이미있다");
       return false;
     }
     // correct

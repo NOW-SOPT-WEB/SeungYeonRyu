d7aa05fc3ca58dd7c8c16809f1bae02cd015caa1
wrryu09 <rryu09@ewhain.net>
2024-04-29 11:59:10 +0900

    fix: prevent custom props rendering

--- week3/src/components/Card.tsx
+++ week3/src/components/Card.tsx
@@ -13,7 +13,7 @@ const Card = (props: Props) => {
 
   return (
     <CardFlipWrapper onClick={flipCard}>
-      <CardContainer flipped={flipped}>
+      <CardContainer $flipped={flipped}>
         <CardBack />
         <CardFront />
       </CardContainer>
@@ -27,13 +27,13 @@ const CardFlipWrapper = styled.div`
   perspective: 1000px;
 `;
 
-const CardContainer = styled.div<{ flipped: boolean }>`
+const CardContainer = styled.div<{ $flipped: boolean }>`
   width: 100%;
   height: 100%;
   position: relative;
   transition: 0.4s;
   transform-style: preserve-3d;
-  transform: ${({ flipped }) => (flipped ? "rotateY(180deg)" : "rotateY(0)")};
+  transform: ${({ $flipped }) => ($flipped ? "rotateY(180deg)" : "rotateY(0)")};
 `;
 
 const CardSide = styled.div`

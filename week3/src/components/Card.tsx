b12f1169fe5daf2e6a6ad27495ba69af6981fa02
wrryu09 <rryu09@ewhain.net>
2024-04-29 11:29:06 +0900

    style: 카드 앞뒷면 제작

--- week3/src/components/Card.tsx
+++ week3/src/components/Card.tsx
@@ -1,15 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
 type Props = {};
 
 const Card = (props: Props) => {
-  return <CardContainer />;
+  const [flipped, setFlipped] = useState(false);
+
+  /** 카드 뒤집기 */
+  const flipCard = () => {
+    setFlipped(!flipped);
+  };
+
+  return (
+    <CardFlipWrapper onClick={flipCard}>
+      <CardContainer flipped={flipped}>
+        <CardBack />
+        <CardFront />
+      </CardContainer>
+    </CardFlipWrapper>
+  );
 };
-const CardContainer = styled.div`
+
+const CardFlipWrapper = styled.div`
   width: 9rem;
   height: 12rem;
-  border-radius: 12px;
-  background-color: lightblue;
+  perspective: 1000px;
 `;
+
+const CardContainer = styled.div<{ flipped: boolean }>`
+  width: 100%;
+  height: 100%;
+  position: relative;
+  transition: 0.4s;
+  transform-style: preserve-3d;
+  transform: ${({ flipped }) => (flipped ? "rotateY(180deg)" : "rotateY(0)")};
+`;
+
+const CardSide = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  border-radius: 12px;
+  backface-visibility: hidden;
+`;
+
+const CardFront = styled(CardSide)`
+  background-color: white;
+`;
+
+const CardBack = styled(CardSide)`
+  background-color: ${({ theme }) => theme.colors.pointGreen};
+  transform: rotateY(180deg);
+`;
+
 export default Card;

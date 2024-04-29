380f3f74e23148d6473ff452c9e9888b59401a1e
wrryu09 <rryu09@ewhain.net>
2024-04-29 12:54:13 +0900

    feat: 카드 데이터 추가

---
+++ week3/src/constants/cardData.ts
@@ -0,0 +1,41 @@
+import { cardImage } from "../assets/cardImage/cardImage";
+
+const cardData = [
+  {
+    id: 1,
+    image: cardImage.cardImg1,
+  },
+  {
+    id: 2,
+    image: cardImage.cardImg2,
+  },
+  {
+    id: 3,
+    image: cardImage.cardImg3,
+  },
+  {
+    id: 4,
+    image: cardImage.cardImg4,
+  },
+  {
+    id: 5,
+    image: cardImage.cardImg5,
+  },
+  {
+    id: 6,
+    image: cardImage.cardImg6,
+  },
+  {
+    id: 7,
+    image: cardImage.cardImg7,
+  },
+  {
+    id: 8,
+    image: cardImage.cardImg8,
+  },
+  {
+    id: 9,
+    image: cardImage.cardImg9,
+  },
+];
+export default cardData;

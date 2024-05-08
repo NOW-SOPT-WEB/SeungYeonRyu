184e439a27fdd10be9d009c83d712898ed5ab8a4
wrryu09 <rryu09@ewhain.net>
2024-05-08 17:34:30 +0900

    refactor: ModalLayout 분리

--- week4/src/pages/Login.tsx
+++ week4/src/pages/Login.tsx
@@ -2,44 +2,23 @@ import styled from "styled-components";
 import CommonBtn from "../components/CommonBtn";
 import LoginInput from "../components/LoginInput";
 import LoginImg from "../assets/images/loginImage.png";
+import ModalLayout from "../components/ModalLayout";
 type Props = {};
 
 const Login = (props: Props) => {
   return (
-    <LoginContainer>
-      <LoginModal>
-        <LoginTitle>Login</LoginTitle>
-        <LoginImage src={LoginImg} />
-        <LoginInput />
-        <LoginBtnWrapper>
-          <CommonBtn text="로그인" />
-          <CommonBtn text="회원가입" link="/join" />
-        </LoginBtnWrapper>
-      </LoginModal>
-    </LoginContainer>
+    <ModalLayout>
+      <LoginTitle>Login</LoginTitle>
+      <LoginImage src={LoginImg} />
+      <LoginInput />
+      <LoginBtnWrapper>
+        <CommonBtn text="로그인" />
+        <CommonBtn text="회원가입" link="/join" />
+      </LoginBtnWrapper>
+    </ModalLayout>
   );
 };
-const LoginContainer = styled.div`
-  display: flex;
-  width: 100%;
-  height: 100vh;
-  justify-content: center;
-  align-items: center;
-  background-color: black;
-`;
-const LoginModal = styled.div`
-  display: flex;
-  width: fit-content;
-  height: fit-content;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
 
-  padding: 6rem;
-  background-color: white;
-  border-radius: 4%;
-  border: solid 3px black;
-`;
 const LoginTitle = styled.h1`
   font-size: 2rem;
   font-weight: 600;

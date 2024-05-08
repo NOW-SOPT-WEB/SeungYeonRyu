19a16f5171a1a8cf941654f3a8b691c8f00ba335
wrryu09 <rryu09@ewhain.net>
2024-05-08 13:37:06 +0900

    refactor: theme 파일 분리

--- week3/src/Theme.tsx
+++ week3/src/Theme.tsx
@@ -1,20 +1,6 @@
 import React from "react";
 import { ThemeProvider } from "styled-components";
-
-const theme = {
-  colors: {
-    mainBlue: "#0085FF",
-    darkGrey: "#353535",
-    pointGreen: "#31F167",
-    magenta: "#FB4CFF",
-  },
-  fonts: ["sans-serif", "Roboto"],
-  fontSizes: {
-    small: "1em",
-    medium: "2em",
-    large: "3em",
-  },
-};
+import theme from "./theme";
 
 const Theme = ({ children }: { children: React.ReactNode }) => {
   return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
---
+++ week3/src/theme.ts
@@ -0,0 +1,15 @@
+const theme = {
+  colors: {
+    mainBlue: "#0085FF",
+    darkGrey: "#353535",
+    pointGreen: "#31F167",
+    magenta: "#FB4CFF",
+  },
+  fonts: ["sans-serif", "Roboto"],
+  fontSizes: {
+    small: "1em",
+    medium: "2em",
+    large: "3em",
+  },
+};
+export default theme;

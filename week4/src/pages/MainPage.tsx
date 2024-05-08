1c5e46aa884d6d1586339b4497c678fac022501b
wrryu09 <rryu09@ewhain.net>
2024-05-08 19:29:47 +0900

    feat: add video

--- week4/src/pages/MainPage.tsx
+++ week4/src/pages/MainPage.tsx
@@ -1,7 +1,13 @@
+import ReactPlayer from "react-player";
+import mainVideo from "../assets/video/mainVideo.mp4";
 type Props = {};
 
 const MainPage = (props: Props) => {
-  return <div>MainPage</div>;
+  return (
+    <div>
+      <ReactPlayer url={mainVideo} loop={true} playing={true} />
+    </div>
+  );
 };
 
 export default MainPage;
